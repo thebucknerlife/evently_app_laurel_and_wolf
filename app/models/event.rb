363fe54a980a8d82e37e3afc74528b083e5b0af0
Greg Buckner <gbuckner23@gmail.com>
2015-05-04 15:27:10 -0700

    added a comment

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,9 +10,9 @@ class Event
   embeds_one :properties
 
   # Callback to convert revenue string into cents and save a revenue_in_cents for summing revenue. 
-  # Works for both #save on new records and .create.
+  # Works for both #save on new records and .create. MongoDB can't sum unless the type is Integer.
   #
-  # Assumes that revenue would never present but blank (e.g. {revenue: ''}). Would need to 
+  # Assumes that revenue would never be present but blank (e.g. {revenue: ''}). Would need to 
   # to test for blankness otherwise.
   before_create do |doc|
     if doc.properties && doc.properties.revenue
